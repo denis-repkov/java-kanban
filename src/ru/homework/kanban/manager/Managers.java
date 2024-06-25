322b937786bc6ee95e7f8e394ee7b2e20282b987
Denis Repkov <denis-toyota2010@yandex.ru>
2024-06-25 18:17:11 +0300

    - исправление по CheckStyle

--- src/ru/homework/kanban/manager/Managers.java
+++ src/ru/homework/kanban/manager/Managers.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 public class Managers {
 
-    private Managers() {}
     public static TaskManager getDefault() {
         new FileBackedTaskManager(getDefaultHistory());
         return FileBackedTaskManager.loadFromFile(new File("resources/backupTasks.csv"));
